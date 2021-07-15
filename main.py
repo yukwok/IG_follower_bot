db61775809fb572ce6243e73a17bcbf14e879d9a
yukwok <billynyk@gmail.com>
2021-07-15 10:54:57 +0800

    some constants

---
+++ main.py
@@ -0,0 +1,5 @@
+# --- some constants
+CHROME_DRIVER_PATH = "/Users/prog/VScode/python/webdev/chromedriver"
+INTERESTED_TAG = "interior design"
+ig_id = "billynyk@gmail.com"
+ig_password = "abcde12345"
